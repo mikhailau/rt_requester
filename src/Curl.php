878714ad54b8de09ecfa421cfc8a762467907a0e
mnv <mnv@cert.by>
2020-05-25 13:17:27 +0300

    composer

--- src/Curl.php
+++ src/Curl.php
@@ -5,10 +5,8 @@ namespace Mikhailau\Rt\Requester;
 class Curl extends \Curl\Curl implements \PetrKnap\Php\Singleton\SingletonInterface
 {
     use \PetrKnap\Php\Singleton\SingletonTrait;
-    public $i=0;
     private function __constructor()
     {
-        echo "HERE";
         parent::__construct();
     }
 
